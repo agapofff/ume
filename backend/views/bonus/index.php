19aa9ea862e232e370f43666158508f05c027f8f
Dmitriy Agapofff <agapofff@gmail.com>
2022-07-19 20:41:25 +0300

    Update index.php

--- backend/views/bonus/index.php
+++ backend/views/bonus/index.php
@@ -156,7 +156,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     
                     [
                         'attribute' => 'created_at',
-                        'format' => ['date', 'php:d F Y'],
+                        'format' => ['date', 'd.m.Y H:i:s'],
                         'filter' => DateRangePicker::widget([
                             'model' => $searchModel,
                             'attribute' => 'created_at',
@@ -187,7 +187,7 @@ $this->params['breadcrumbs'][] = $this->title;
                     
                     [
                         'attribute' => 'updated_at',
-                        'format' => ['date', 'php:d F Y'],
+                        'format' => ['date', 'd.m.Y H:i:s'],
                         'filter' => DateRangePicker::widget([
                             'model' => $searchModel,
                             'attribute' => 'updated_at',
