bb7714f9e2939b7f68d2bc26b112764f80340000
Dmitriy Agapofff <agapofff@gmail.com>
2022-11-09 05:30:21 +0300

    Update index.php

--- frontend/views/news/index.php
+++ frontend/views/news/index.php
@@ -5,7 +5,7 @@ $this->title = Yii::t('front', 'Новости');
 ?>
 
 <div class="container-lg container-xl container-xxl">
-    <h1 class="mb-3 text-uppercase">
+    <h1 class="mb-3 text-uppercase font-weight-light">
         <?= Yii::t('front', 'Новости') ?>
     </h1>
     <div id="news" class="row mb-3">
