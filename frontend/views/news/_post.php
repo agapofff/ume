8c55f343d11f8c2b9efbee1ebadd84b8b990164b
Dmitriy Agapofff <agapofff@gmail.com>
2022-11-18 04:35:23 +0300

    news

--- frontend/views/news/_post.php
+++ frontend/views/news/_post.php
@@ -8,9 +8,9 @@ $cachedImage = '/images/cache/News/News' . $image->itemId . '/' . $image->urlAli
 $name = json_decode($post->name)->{Yii::$app->language};
 ?>
 
-<a href="<?= Url::to(['/news/' . $post->slug]) ?>" class="news-post card rounded-0 border-gray-800 mb-2 transition text-dark overflow-hidden">
+<a href="<?= Url::to(['/news/' . $post->slug]) ?>" class="news-post card rounded-0 border-gray-600 mb-2 transition text-dark overflow-hidden">
     <div class="news-img-zoom">
-        <img src="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImage) ? $cachedImage : $image->getUrl('500x500') ?>" alt="<?= $image->alt ?: $name ?>" class="card-img transition">
+        <img src="<?= file_exists(Yii::getAlias('@frontend') . '/web' . $cachedImage) ? $cachedImage : $image->getUrl('500x500') ?>" alt="<?= $image->alt ?: $name ?>" class="card-img transition rounded-0">
     </div>
     <div class="card-img-overlay p-1_5">
         <p class="h6 blog-post-date mb-2 opacity-50 small">
@@ -19,7 +19,7 @@ $name = json_decode($post->name)->{Yii::$app->language};
         <p class="h5 font-weight-500 mb-2">
             <?= $name ?>
         </p>
-        <p class="mb-0 lead text-white d-sm-none d-md-block d-lg-none d-xl-block">
+        <p class="mb-0 lead text-dark d-sm-none d-md-block d-lg-none d-xl-block">
             <?= json_decode($post->description)->{Yii::$app->language} ?>
         </p>
     </div>
