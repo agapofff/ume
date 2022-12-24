8e85ff2c75382426bf77e94582a9dd690295e74e
Dmitriy Agapofff <agapofff@gmail.com>
2022-12-24 21:54:24 +0300

    Update elementListRow.php

--- vendor/dvizh/yii2-cart/src/widgets/views/elementListRow.php
+++ vendor/dvizh/yii2-cart/src/widgets/views/elementListRow.php
@@ -27,48 +27,144 @@ if ($options && !empty($allOptions)) {
 
 ?>
 
-<div class="cart-product" data-product-id="<?= $model->item_id ?>" data-currency="<?= $currency ?>" data-id="<?= $model->comment ?>" data-name="<?= $name ?>" data-price="<?= round($model->price) ?>">
-    <div class="row justify-content-between">
-        <div class="col">
+<?php
+    if (Yii::$app->devicedetect->isMobile()) {
+?>
+        <div class="cart-product" data-product-id="<?= $model->item_id ?>" data-currency="<?= $currency ?>" data-id="<?= $model->comment ?>" data-name="<?= $name ?>" data-price="<?= round($model->price) ?>">
             <div class="row">
-                <div class="col-auto">
+                <div class="col-auto r-0">
                     <a href="<?= $url ?>" class="d-block border border-gray-400 text-center" style="
-                        width: 150px; 
-                        height: 150px;
+                        width: 100px; 
+                        height: 100px;
                         background: url('<?= $image ?>') center center / contain no-repeat;
                     ">
                     </a>
                 </div>
                 <div class="col">
-                    <div class="row h-100">
-                        <div class="col-12 align-self-start">
-                            <p class="font-weight-bold mt-1">
-                                <?= $name ?> <?= $optionValue ?>
-                            </p>
-                            
-                        <?php
-                            if ($model->item_id == Yii::$app->params['gift']['product_id']) {
-                        ?>
-                                <p class="text-danger"><?= Yii::t('front', 'Подарок') ?></p>
-                        <?php
+                    <p class="font-weight-bold mt-1">
+                        <?= $name ?> <?= $optionValue ?>
+                    </p>
+                    
+                <?php
+                    if ($model->item_id == Yii::$app->params['gift']['product_id']) {
+                ?>
+                        <p class="text-danger"><?= Yii::t('front', 'Подарок') ?></p>
+                <?php
+                    }
+                ?>
+                    
+                <?php 
+                    if (!empty($otherFields)) {
+                        foreach ($otherFields as $fieldName => $field) {
+                            if (isset($product->$field)){
+                                echo Html::tag('p', $fieldName . ': ' . Html::tag('strong', $product->$field));
                             }
-                        ?>
-                            
-                            <?php 
-                                if (!empty($otherFields)) {
-                                    foreach ($otherFields as $fieldName => $field) {
-                                        if (isset($product->$field)){
-                                            echo Html::tag('p', $fieldName . ': ' . Html::tag('strong', $product->$field));
-                                        }
+                        }
+                    }
+                ?>
+                </div>
+            </div>
+            <div class="row juatify-content-between align-items-center">
+                <div class="col-auto">
+                    <?= ElementPrice::widget([
+                            'model' => $model,
+                            'currency' => $currency,
+                            'htmlTag' => 'h4',
+                            'cssClass' => 'font-weight-normal text-nowrap mb-0 d-inline text-right',
+                        ]);
+                    ?>
+                </div>
+                <div class="col-auto">
+                    <?= ElementPrice::widget([
+                            'model' => $model,
+                            'currency' => $currency,
+                            'htmlTag' => 'h4',
+                            'cssClass' => 'font-weight-normal text-nowrap mb-0 d-inline text-right',
+                        ]);
+                    ?>
+                </div>
+                <div class="col-auto">
+                    <?= DeleteButton::widget([
+                            'model' => $model,
+                            'deleteElementUrl' => Url::to([$controllerActions['delete']]),
+                            'lineSelector' => 'list-group-item',
+                            'cssClass' => 'delete cart-delete',
+                            'text' => '<img src="/images/cart_delete.svg" style="width:16px">',
+                        ])
+                    ?>
+                </div>
+            </div>
+        </div>
+<?php
+    } else {
+?>
+        <div class="cart-product" data-product-id="<?= $model->item_id ?>" data-currency="<?= $currency ?>" data-id="<?= $model->comment ?>" data-name="<?= $name ?>" data-price="<?= round($model->price) ?>">
+            <div class="row justify-content-between">
+                <div class="col">
+                    <div class="row">
+                        <div class="col-auto">
+                            <a href="<?= $url ?>" class="d-block border border-gray-400 text-center" style="
+                                width: 150px; 
+                                height: 150px;
+                                background: url('<?= $image ?>') center center / contain no-repeat;
+                            ">
+                            </a>
+                        </div>
+                        <div class="col">
+                            <div class="row h-100">
+                                <div class="col-12 align-self-start">
+                                    <p class="font-weight-bold mt-1">
+                                        <?= $name ?> <?= $optionValue ?>
+                                    </p>
+                                    
+                                <?php
+                                    if ($model->item_id == Yii::$app->params['gift']['product_id']) {
+                                ?>
+                                        <p class="text-danger"><?= Yii::t('front', 'Подарок') ?></p>
+                                <?php
                                     }
-                                }
+                                ?>
+                                    
+                                    <?php 
+                                        if (!empty($otherFields)) {
+                                            foreach ($otherFields as $fieldName => $field) {
+                                                if (isset($product->$field)){
+                                                    echo Html::tag('p', $fieldName . ': ' . Html::tag('strong', $product->$field));
+                                                }
+                                            }
+                                        }
+                                    ?>
+                                </div>
+                                <div class="col-12 align-self-end">
+                                    <?= ChangeCount::widget([
+                                            'model' => $model,
+                                            'showArrows' => $showCountArrows,
+                                            'actionUpdateUrl' => Url::to([$controllerActions['update']]),
+                                        ]);
+                                    ?>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-auto">
+                    <div class="row h-100">
+                        <div class="col-12 align-self-start text-right mt-1">
+                            <?= DeleteButton::widget([
+                                    'model' => $model,
+                                    'deleteElementUrl' => Url::to([$controllerActions['delete']]),
+                                    'lineSelector' => 'list-group-item',
+                                    'cssClass' => 'delete cart-delete',
+                                    'text' => '<img src="/images/cart_delete.svg" style="width:16px">',
+                                ])
                             ?>
                         </div>
-                        <div class="col-12 align-self-end">
-                            <?= ChangeCount::widget([
+                        <div class="col-12 align-self-end text-right mb-0_5">
+                            <?= ElementPrice::widget([
                                     'model' => $model,
-                                    'showArrows' => $showCountArrows,
-                                    'actionUpdateUrl' => Url::to([$controllerActions['update']]),
+                                    'currency' => $currency,
+                                    'htmlTag' => 'h4',
+                                    'cssClass' => 'font-weight-normal text-nowrap mb-0 d-inline text-right',
                                 ]);
                             ?>
                         </div>
@@ -76,31 +172,8 @@ if ($options && !empty($allOptions)) {
                 </div>
             </div>
         </div>
-		<div class="col-auto">
-			<div class="row h-100">
-				<div class="col-12 align-self-start text-right mt-1">
-					<?= DeleteButton::widget([
-							'model' => $model,
-							'deleteElementUrl' => Url::to([$controllerActions['delete']]),
-							'lineSelector' => 'list-group-item',
-							'cssClass' => 'delete cart-delete',
-							'text' => '<img src="/images/cart_delete.svg" style="width:16px">',
-						])
-					?>
-				</div>
-				<div class="col-12 align-self-end text-right mb-0_5">
-                    <?= ElementPrice::widget([
-							'model' => $model,
-							'currency' => $currency,
-							'htmlTag' => 'h4',
-							'cssClass' => 'font-weight-normal text-nowrap mb-0 d-inline text-right',
-						]);
-                    ?>
-
-				</div>
-			</div>
-		</div>
-	</div>
-</div>
+<?php
+    }
+?>
 
 <hr class="my-1_5">
