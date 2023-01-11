8de4f81743fae6890c4972bc2b3d3f130edb5a7a
Dmitriy Agapofff <agapofff@gmail.com>
2023-01-11 18:34:56 +0300

    Update index.php

--- frontend/web/index.php
+++ frontend/web/index.php
@@ -3,8 +3,8 @@
     error_reporting(E_ALL ^ E_NOTICE);
     // ini_set('display_errors', 1);
     // ini_set('display_startup_errors', 1);
-	defined('YII_DEBUG') or define('YII_DEBUG', false);
-    defined('YII_ENV') or define('YII_ENV', 'prod');
+	defined('YII_DEBUG') or define('YII_DEBUG', true);
+    defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
