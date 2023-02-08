30e0472eb0335801f2145460d98ed422962a0550
Dmitriy Agapofff <agapofff@gmail.com>
2023-02-08 13:17:39 +0300

    Update index.php

--- backend/web/index.php
+++ backend/web/index.php
@@ -1,6 +1,6 @@
 <?php
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+defined('YII_DEBUG') or define('YII_DEBUG', false);
+defined('YII_ENV') or define('YII_ENV', 'prod');
 
 error_reporting(E_ALL & ~E_NOTICE);
 
