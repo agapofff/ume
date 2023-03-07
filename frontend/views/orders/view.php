89fef70df4089755a955c0dd1f9def995fad9f1f
Dmitriy Agapofff <agapofff@gmail.com>
2023-03-07 18:40:05 +0300

    Update view.php

--- frontend/views/orders/view.php
+++ frontend/views/orders/view.php
@@ -18,147 +18,149 @@
 <?php
     if ($order) {
 ?>
-    <div id="order-content">
-        <div class="row my-4 my-lg-5">
-            <div class="col-12 col-md-6">
-                <div class="dl-horizontal">
-                    <dt>
-                        <?= Yii::t('front', 'Дата') ?>
-                    </dt>
-                    <dd>
-                        <?= Yii::$app->formatter->asDate($order->date) ?>, <?= Yii::$app->formatter->asTime($order->time) ?>
-                    </dd>
-                    <dt>
-                        <?= Yii::t('front', 'Статус') ?>
-                    </dt>
-                    <dd>
-                        <?= Yii::$app->getModule('order')->orderStatuses[$order->status] ?>
-                    </dd>
-                </div>
-                <div class="dl-horizontal">
-                    <dt>
-                        <?= Yii::t('front', 'Имя') ?>
-                    </dt>
-                    <dd>
-                        <?= $order->client_name ?>
-                    </dd>
-                    <dt>
-                        <?= Yii::t('front', 'Email') ?>
-                    </dt>
-                    <dd>
-                        <?= $order->email ?>
-                    </dd>
-                    <dt>
-                        <?= Yii::t('front', 'Телефон') ?>
-                    </dt>
-                    <dd>
-                        <?= $order->phone ?>
-                    </dd>
-                </div>
-                <div class="dl-horizontal">
-                    <dt>
-                        <?= Yii::t('front', 'Способ доставки') ?>
-                    </dt>
-                    <dd>
-                        <?= Yii::t('front', ArrayHelper::getValue(ArrayHelper::map($shippingTypes, 'id', 'name'), $order->shipping_type_id)) ?>
-                    </dd>
-                </div>
-            </div>
-            <div class="col-12 col-md-6">
-                <div class="dl-horizontal">
-            <?php
-                foreach ($fields as $field) {
-                    if ($fieldValue = ArrayHelper::getValue(ArrayHelper::map($fieldValues, 'field_id', 'value'), $field->id)) {
-            ?>
+    <div class="my-4 my-lg-5">
+        <div id="order-content">
+            <div class="row">
+                <div class="col-12 <?= Yii::$app->request->isAjax ? '' : 'col-md-6' ?>">
+                    <div class="dl-horizontal">
                         <dt>
-                            <?= Yii::t('front', $field->description) ?>
+                            <?= Yii::t('front', 'Дата') ?>
                         </dt>
                         <dd>
-                            <?= $field->id == 11 ? Yii::$app->formatter->asCurrency($fieldValue, $currency) : $fieldValue ?>
+                            <?= Yii::$app->formatter->asDate($order->date) ?>, <?= Yii::$app->formatter->asTime($order->time) ?>
                         </dd>
-            <?php
-                        if ($field->id == 8 && $order->address) {
-            ?>
+                        <dt>
+                            <?= Yii::t('front', 'Статус') ?>
+                        </dt>
+                        <dd>
+                            <?= Yii::$app->getModule('order')->orderStatuses[$order->status] ?>
+                        </dd>
+                    </div>
+                    <div class="dl-horizontal">
+                        <dt>
+                            <?= Yii::t('front', 'Имя') ?>
+                        </dt>
+                        <dd>
+                            <?= $order->client_name ?>
+                        </dd>
+                        <dt>
+                            <?= Yii::t('front', 'Email') ?>
+                        </dt>
+                        <dd>
+                            <?= $order->email ?>
+                        </dd>
+                        <dt>
+                            <?= Yii::t('front', 'Телефон') ?>
+                        </dt>
+                        <dd>
+                            <?= $order->phone ?>
+                        </dd>
+                    </div>
+                    <div class="dl-horizontal">
+                        <dt>
+                            <?= Yii::t('front', 'Способ доставки') ?>
+                        </dt>
+                        <dd>
+                            <?= Yii::t('front', ArrayHelper::getValue(ArrayHelper::map($shippingTypes, 'id', 'name'), $order->shipping_type_id)) ?>
+                        </dd>
+                    </div>
+                </div>
+                <div class="col-12 <?= Yii::$app->request->isAjax ? '' : 'col-md-6' ?>">
+                    <div class="dl-horizontal">
+                <?php
+                    foreach ($fields as $field) {
+                        if ($fieldValue = ArrayHelper::getValue(ArrayHelper::map($fieldValues, 'field_id', 'value'), $field->id)) {
+                ?>
                             <dt>
-                                <?= Yii::t('front', 'Адрес') ?>
+                                <?= Yii::t('front', $field->description) ?>
                             </dt>
                             <dd>
-                                <?= $order->address ?>
-                            </dd>        
-            <?php
+                                <?= $field->id == 11 ? Yii::$app->formatter->asCurrency($fieldValue, $currency) : $fieldValue ?>
+                            </dd>
+                <?php
+                            if ($field->id == 8 && $order->address) {
+                ?>
+                                <dt>
+                                    <?= Yii::t('front', 'Адрес') ?>
+                                </dt>
+                                <dd>
+                                    <?= $order->address ?>
+                                </dd>        
+                <?php
+                            }
                         }
                     }
-                }
-            ?>
+                ?>
 
+                    </div>
                 </div>
-            </div>
 
-            <div class="col-12 my-4 my-lg-5 table-responsive">
-                <table class="table">
-                    <thead>
-                        <tr>
-                            <th></th>
-                            <th class="text-left">
-                                <?= Yii::t('front', 'Товар') ?>
-                            </th>
-                            <th class="text-center">
-                                <?= Yii::t('front', 'Количество') ?>
-                            </th>
-                            <th class="text-center">
-                                <?= Yii::t('front', 'Цена') ?>
-                            </th>
-                            <th class="text-center">
-                                <?= Yii::t('front', 'Итого') ?>
-                            </th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    <?php
-                        foreach ($elements as $key => $element) {
-                            $product_link = Url::to(['/product/' . $element->product->slug], true);
-                    ?>
-                        <tr>
-                            <td class="text-right">
-                                <a href="<?= $product_link ?>">
-                                    <img src="<?= Url::to($element->product->getImage()->getUrl('60x60'), true) ?>">
-                                </a>
-                            </td>
-                            <td class="text-left" style="min-width: 160px;">
-                                <p>
+                <div class="col-12 my-4 my-lg-5 table-responsive">
+                    <table class="table">
+                        <thead>
+                            <tr>
+                                <th></th>
+                                <th class="text-left">
+                                    <?= Yii::t('front', 'Товар') ?>
+                                </th>
+                                <th class="text-center">
+                                    <?= Yii::t('front', 'Количество') ?>
+                                </th>
+                                <th class="text-center">
+                                    <?= Yii::t('front', 'Цена') ?>
+                                </th>
+                                <th class="text-center">
+                                    <?= Yii::t('front', 'Итого') ?>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        <?php
+                            foreach ($elements as $key => $element) {
+                                $product_link = Url::to(['/product/' . $element->product->slug], true);
+                        ?>
+                            <tr>
+                                <td class="text-right">
                                     <a href="<?= $product_link ?>">
-                                        <strong>
-                                            <?= json_decode($element->name)->{$lang} ?> <?= $sizes[$key] ?>
-                                        </strong>
+                                        <img src="<?= Url::to($element->product->getImage()->getUrl('60x60'), true) ?>">
                                     </a>
-                                </p>
-                            </td>
-                            <td class="text-center align-middle">
-                                <?= (int)$element->count ?>
-                            </td>
-                            <td class="text-center align-middle">
-                                <?= Yii::$app->formatter->asCurrency($element->price, $currency) ?>
-                            </td>
-                            <td class="text-center align-middle">
-                                <?= Yii::$app->formatter->asCurrency(($element->count * $element->price), $currency) ?>
-                            </td>
-                        </tr>
-                    <?php
-                        }
-                    ?>
-                    </tbody>
-                    <tfoot>
-                        <tr>
-                            <td colspan="3"></td>
-                            <td class="text-center">
-                                <strong><?= Yii::t('front', 'Итого') ?></strong>:
-                            </td>
-                            <td class="text-center">
-                                <?= Yii::$app->formatter->asCurrency($order->cost, $currency) ?>
-                            </td>
-                        </tr>
-                    </tfoot>
-                </table>
+                                </td>
+                                <td class="text-left" style="min-width: 160px;">
+                                    <p>
+                                        <a href="<?= $product_link ?>">
+                                            <strong>
+                                                <?= json_decode($element->name)->{$lang} ?> <?= $sizes[$key] ?>
+                                            </strong>
+                                        </a>
+                                    </p>
+                                </td>
+                                <td class="text-center align-middle">
+                                    <?= (int)$element->count ?>
+                                </td>
+                                <td class="text-center align-middle">
+                                    <?= Yii::$app->formatter->asCurrency($element->price, $currency) ?>
+                                </td>
+                                <td class="text-center align-middle">
+                                    <?= Yii::$app->formatter->asCurrency(($element->count * $element->price), $currency) ?>
+                                </td>
+                            </tr>
+                        <?php
+                            }
+                        ?>
+                        </tbody>
+                        <tfoot>
+                            <tr>
+                                <td colspan="3"></td>
+                                <td class="text-center">
+                                    <strong><?= Yii::t('front', 'Итого') ?></strong>:
+                                </td>
+                                <td class="text-center">
+                                    <?= Yii::$app->formatter->asCurrency($order->cost, $currency) ?>
+                                </td>
+                            </tr>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
